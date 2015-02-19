cbc860974f07346715192ae7346bca4340ff2918
Michael Basil <michael@intrications.com>
2015-02-19 08:38:36 +0000

    Switch to using PreferenceFragment

--- app/src/main/java/uk/co/ashtonbrsc/intentexplode/Settings.java
+++ app/src/main/java/uk/co/ashtonbrsc/intentexplode/Settings.java
@@ -14,59 +14,18 @@
 
 package uk.co.ashtonbrsc.intentexplode;
 
-import android.content.ComponentName;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.preference.Preference;
-import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.PreferenceActivity;
-import android.support.v4.app.ShareCompat;
+import android.support.v7.app.ActionBarActivity;
 
 import uk.co.ashtonbrsc.android.intentintercept.R;
 
-public class Settings extends PreferenceActivity implements
-		OnPreferenceChangeListener {
+public class Settings extends ActionBarActivity {
 
-    private Preference interceptEnabledPreference;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-
-		addPreferencesFromResource(R.xml.settings);
-
-		interceptEnabledPreference = findPreference(getString(R.string.intercept_enable_pref));
-
-		interceptEnabledPreference.setOnPreferenceChangeListener(this);
-
-        Preference sendTestIntentPreference = findPreference(getString(R.string.send_test_intent_pref));
-        sendTestIntentPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(Preference preference) {
-                Intent intent = ShareCompat.IntentBuilder.from(Settings.this).setChooserTitle
-                        ("Select Intent Intercept").setType("plain/text")
-                        .setText("Test Intent").createChooserIntent();
-                startActivity(intent);
-                return true;
-            }
-        });
-
-	}
-
-	public boolean onPreferenceChange(Preference preference, Object newValue) {
-		if (preference == interceptEnabledPreference) {
-
-			Boolean enabled = (Boolean) newValue;
-			int flag = (enabled ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED
-					: PackageManager.COMPONENT_ENABLED_STATE_DISABLED);
-			ComponentName component = new ComponentName(Settings.this,
-					Explode.class);
-
-			getPackageManager().setComponentEnabledSetting(component, flag,
-					PackageManager.DONT_KILL_APP);
-		}
-		return true;
-	}
-
+        getFragmentManager().beginTransaction().replace(R.id.content_frame, new SettingsFragment()).commit();
+    }
 }
