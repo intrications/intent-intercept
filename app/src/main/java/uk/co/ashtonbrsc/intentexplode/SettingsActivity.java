b80adf538593b3c5c5de9f30ee4e1d01c16e4c8a
Michael Basil <michael@intrications.com>
2015-02-20 07:51:11 +0000

    Restore minSDK to 7

--- app/src/main/java/uk/co/ashtonbrsc/intentexplode/SettingsActivity.java
+++ app/src/main/java/uk/co/ashtonbrsc/intentexplode/SettingsActivity.java
@@ -14,13 +14,16 @@
 
 package uk.co.ashtonbrsc.intentexplode;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
 import uk.co.ashtonbrsc.android.intentintercept.R;
 
-public class Settings extends ActionBarActivity {
+public class SettingsActivity extends ActionBarActivity {
 
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
